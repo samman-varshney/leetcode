ad18c6acd0a6d91b559b0be8b743aa705287d5c6
Samman Varshney <sammanvarshney345@gmail.com>
2025-11-23 22:21:59 +0530

    Time: 225 ms (89.83%) | Memory: 272.1 MB (77.65%) - LeetSync

--- 3946-find-maximum-balanced-xor-subarray-length/find-maximum-balanced-xor-subarray-length.java
+++ 3946-find-maximum-balanced-xor-subarray-length/find-maximum-balanced-xor-subarray-length.java
@@ -2,8 +2,7 @@ class Solution {
     public int maxBalancedSubarray(int[] nums) {
         int n = nums.length;
         HashMap<Long, Integer> map = new HashMap<>();
-        map.put((long)n, -1);
-        long offset = n;
+        map.put(0L, -1);
         long xor = 0, odd= 0, even = 0;
         int res = 0;
         for(int i=0; i<n; i++){
@@ -13,7 +12,7 @@ class Solution {
             }else{
                 odd++;
             }
-            long key = (xor<<32)|(even-odd+offset);
+            long key = (xor<<32)|((even-odd)&0xffffffffL);
             if(map.containsKey(key)){
                 res = Math.max(i - map.get(key), res);
             }else{
