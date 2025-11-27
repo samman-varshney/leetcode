93ae167e117132d70eee61a2fe2468cb8f53430e
Samman Varshney <sammanvarshney345@gmail.com>
2025-11-27 23:33:49 +0530

    Time: 45 ms (45.45%) | Memory: 82.9 MB (17.46%) - LeetSync

--- 2725-mice-and-cheese/mice-and-cheese.java
+++ 2725-mice-and-cheese/mice-and-cheese.java
@@ -1,36 +1,27 @@
 class Solution {
     public int miceAndCheese(int[] r1, int[] r2, int k) {
         int n = r1.length;
-        int[][] nums = new int[n][2];
-        for(int i=0; i<n; i++){
-            nums[i][0] = r1[i];
-            nums[i][1] = r2[i];
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = r1[i];
+            arr[i][1] = r1[i] - r2[i]; // diff
         }
 
-        Arrays.sort(nums, (a, b)->{
-            int d1 = Math.abs(a[1]-a[0]), d2 = Math.abs(b[1]-b[0]);
-            // if(d1 != d2){
-                return d1 - d2;
-            // }else{
-            //     return Math.max(a[0], a[1]) - Math.max(b[0], b[1]);
-            // }
-        });
+        Arrays.sort(arr, (a, b) -> b[1] - a[1]); // sort by diff descending
 
-        
         int sum = 0;
-        int equals = 0;
-        for(int i=n-1; i>=0; i--){
-            if( i>= k){
-                if(nums[i][0] > nums[i][1] && k > 0){
-                    k--;
-                    sum += nums[i][0];
-                }else{
-                    sum += nums[i][1];
-                }
-            }else{
-                sum += nums[i][0];
-            }
+
+        // give the first k items to mouse 1
+        for (int i = 0; i < k; i++) {
+            sum += arr[i][0];
         }
+
+        // remaining to mouse 2
+        for (int i = k; i < n; i++) {
+            sum += (arr[i][0] - arr[i][1]);
+        }
+
         return sum;
     }
 }
