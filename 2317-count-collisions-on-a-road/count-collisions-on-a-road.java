b6850400a22a0bd0568065dfd9c3a0a32a524512
Samman Varshney <sammanvarshney345@gmail.com>
2025-12-05 18:15:18 +0530

    Time: 17 ms (46.99%) | Memory: 47.7 MB (27.31%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -7,14 +7,16 @@ class Solution {
             if(c == 'R'){
                 r+=1;
                 s = 0;
-            }else if(r != 0){
-                res += c == 'L'?r+1:r;
-                r = 0;
-                s = 1;
-            }else if(s== 1 && c == 'L'){
-                res += 1;
-            }else if(c == 'S'){
-                s = 1;
+            }else{
+                if(r != 0){
+                    res += c=='L'?r+1:r;
+                    r = 0;
+                    s = 1;
+                }else if(s == 1 && c == 'L'){
+                    res+=1;
+                }else if(c == 'S'){
+                    s = 1;
+                }
             }
         }
         return res;
