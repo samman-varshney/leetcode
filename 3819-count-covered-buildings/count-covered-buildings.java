a17c4da109023173e09b77564ab1b77d0dc7b23d
Samman Varshney <sammanvarshney345@gmail.com>
2025-12-11 11:46:59 +0530

    Time: 263 ms (38.95%) | Memory: 239.3 MB (53.69%) - LeetSync

--- 3819-count-covered-buildings/count-covered-buildings.java
+++ 3819-count-covered-buildings/count-covered-buildings.java
@@ -1,24 +1,52 @@
 class Solution {
-    public int countCoveredBuildings(int n, int[][] buildings) {
-        HashMap<Integer, TreeSet<Integer>> xAxis = new HashMap<>();
-        HashMap<Integer, TreeSet<Integer>> yAxis = new HashMap<>();
-
-
-        for(int[] point: buildings){
-            xAxis.computeIfAbsent(point[0], k->new TreeSet()).add(point[1]);
-            yAxis.computeIfAbsent(point[1], k->new TreeSet()).add(point[0]);
+    public int countCoveredBuildings(int m, int[][] buildings) {
+        int n = buildings.length;
+        int[][] nums = new int[n][3];
+        for(int i=0; i<n; i++){
+            nums[i][0] = buildings[i][0];
+            nums[i][1] = buildings[i][1];
         }
 
+        Arrays.sort(nums, (a, b)->{
+            if(a[0]!=b[0])
+                return a[0]-b[0];
+            return a[1] - b[1];
+        });
+
+        for(int i=1; i<n; i++){
+            if(nums[i][0] == nums[i-1][0]){
+                nums[i][2]++;
+            }
+        }
+        for(int i=n-2; i>=0; i--){
+            if(nums[i][0] == nums[i+1][0]){
+                nums[i][2]++;
+            }
+        }
+
+        Arrays.sort(nums, (a, b)->{
+            if(a[1]!=b[1])
+                return a[1] - b[1];
+            return a[0] - b[0];
+        });
+
+        for(int i=1; i<n; i++){
+            if(nums[i][1] == nums[i-1][1]){
+                nums[i][2]++;
+            }
+        }
+        for(int i=n-2; i>=0; i--){
+            if(nums[i][1] == nums[i+1][1]){
+                nums[i][2]++;
+            }
+        }
         int count = 0;
-        for(int[] point: buildings){
-            TreeSet<Integer> horizontal = xAxis.get(point[0]);
-            if(horizontal.ceiling(point[1]+1)==null || horizontal.floor(point[1]-1)==null)
-                continue;
-            TreeSet<Integer> vertical  = yAxis.get(point[1]);
-            if(vertical.ceiling(point[0]+1)==null || vertical.floor(point[0]-1)==null)
-                continue;
-            count++;
+        for(int i=0; i<n; i++){
+            if(nums[i][2] == 4){
+                count++;
+            }
         }
         return count;
+
     }
 }
