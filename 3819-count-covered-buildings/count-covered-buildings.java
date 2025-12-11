7687f2f6fc5bc3e7f39545ecdbe6abb3a3917e97
Samman Varshney <sammanvarshney345@gmail.com>
2025-12-11 11:48:40 +0530

    Time: 260 ms (38.95%) | Memory: 239.4 MB (53.69%) - LeetSync

--- 3819-count-covered-buildings/count-covered-buildings.java
+++ 3819-count-covered-buildings/count-covered-buildings.java
@@ -13,16 +13,16 @@ class Solution {
             return a[1] - b[1];
         });
 
-        for(int i=1; i<n; i++){
+        for(int i=1; i<n-1; i++){
             if(nums[i][0] == nums[i-1][0]){
                 nums[i][2]++;
             }
-        }
-        for(int i=n-2; i>=0; i--){
             if(nums[i][0] == nums[i+1][0]){
                 nums[i][2]++;
             }
+            
         }
+       
 
         Arrays.sort(nums, (a, b)->{
             if(a[1]!=b[1])
@@ -30,16 +30,15 @@ class Solution {
             return a[0] - b[0];
         });
 
-        for(int i=1; i<n; i++){
+        for(int i=1; i<n-1; i++){
             if(nums[i][1] == nums[i-1][1]){
                 nums[i][2]++;
             }
-        }
-        for(int i=n-2; i>=0; i--){
             if(nums[i][1] == nums[i+1][1]){
                 nums[i][2]++;
             }
         }
+       
         int count = 0;
         for(int i=0; i<n; i++){
             if(nums[i][2] == 4){
