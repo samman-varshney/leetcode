dc7da970235c0b1916d04b7550bac0763b6dda29
Samman Varshney <sammanvarshney345@gmail.com>
2025-12-12 12:17:13 +0530

    Time: 1302 ms (5.03%) | Memory: 269.7 MB (10.83%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.java
+++ 3885-count-special-triplets/count-special-triplets.java
@@ -1,44 +1,24 @@
 class Solution {
     public int specialTriplets(int[] nums) {
+        int n = nums.length;
+        int mod = (int)1e9+7;
 
-        int n = nums.length, mod = (int)1e9+7;
-        TreeSet<int[]> set = new TreeSet<>((a, b)->(a[0]-b[0]));
-        long[] left = new long[n];
-        long[] right = new long[n];
+        long[] map = new long[1000000];
+        long left[] = new long[n];
 
         for(int i=0; i<n; i++){
-            int[] candidate = set.ceiling(new int[]{nums[i]*2, 0});
-            if(candidate!=null && candidate[0] == nums[i]*2){
-                left[i] = candidate[1];
-            }
+            left[i] = map[nums[i]*2];
+            map[nums[i]]++;
+        } 
 
-            candidate = set.ceiling(new int[]{nums[i], 0});
-            if(candidate!=null && candidate[0] == nums[i]){
-                candidate[1]++;
-            }else{
-                set.add(new int[]{nums[i], 1});
-            }
-        }
-
-        set.clear();
+        Arrays.fill(map, 0);
+        long res = 0;
 
         for(int i=n-1; i>=0; i--){
-            int[] candidate = set.ceiling(new int[]{nums[i]*2, 0});
-            if(candidate!=null && candidate[0] == nums[i]*2){
-                right[i] = candidate[1];
-            }
+            res = (res + left[i] * map[nums[i]*2])%mod;
+            map[nums[i]]++;
+        }
 
-            candidate = set.ceiling(new int[]{nums[i], 0});
-            if(candidate!=null && candidate[0] == nums[i]){
-                candidate[1]++;
-            }else{
-                set.add(new int[]{nums[i], 1});
-            }
-        }
-        long res = 0;
-        for(int i=0; i<n; i++){
-            res = (res+ left[i]*right[i])%mod;
-        }
         return (int)res;
     }
 }
