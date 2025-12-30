064f972a09f370d6f8d503bfe3cfbb8a2ceb65e3
Samman Varshney <sammanvarshney345@gmail.com>
2025-12-30 12:37:39 +0530

    Time: 10 ms (27.26%) | Memory: 46.5 MB (49.75%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,45 +1,62 @@
-class Pair{
+class Triplet {
+    int city;
+    int price;
+    int stops;
+
+    public Triplet(int city, int price, int stops) {
+        this.city = city;
+        this.price = price;
+        this.stops = stops;
+    }
+}
+
+class Pair {
     int city;
     int price;
 
-    public Pair(int city, int price){
+    public Pair(int city, int price) {
         this.city = city;
         this.price = price;
     }
 }
+
 class Solution {
-
-
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+
         List<List<Pair>> adj = new ArrayList<>();
-        for(int i=0; i<n; i++)
-            adj.add(new ArrayList<>());
-        
-        for(int[] f: flights){
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+
+        for (int[] f : flights) {
             adj.get(f[0]).add(new Pair(f[1], f[2]));
         }
 
-        int[] prices = new int[n];
-        Arrays.fill(prices, Integer.MAX_VALUE);
-        prices[src] = 0;
-        Queue<Pair> pq = new LinkedList<>();
-        pq.add(new Pair(src, 0));
-        k++;
-        while(!pq.isEmpty() && k>0){
-            int size = pq.size();
-            while(size-- >0){
+        int[][] dist = new int[n][k + 2];
+        for (int i = 0; i < n; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
 
-                Pair p = pq.poll();
-                
-                for(Pair nbrs: adj.get(p.city)){
-                    if(prices[nbrs.city] > p.price + nbrs.price){
-                        prices[nbrs.city] = p.price + nbrs.price;
-                        pq.add(new Pair(nbrs.city, prices[nbrs.city]));
-                    }
+        PriorityQueue<Triplet> pq =
+                new PriorityQueue<>((a, b) -> a.price - b.price);
+
+        pq.add(new Triplet(src, 0, 0));
+        dist[src][0] = 0;
+
+        while (!pq.isEmpty()) {
+            Triplet cur = pq.poll();
+
+            if (cur.city == dst) return cur.price;
+
+            if (cur.stops == k + 1) continue;
+
+            for (Pair nbr : adj.get(cur.city)) {
+                int newCost = cur.price + nbr.price;
+                int newStops = cur.stops + 1;
+
+                if (newCost < dist[nbr.city][newStops]) {
+                    dist[nbr.city][newStops] = newCost;
+                    pq.add(new Triplet(nbr.city, newCost, newStops));
                 }
             }
-            k--;
         }
-        return prices[dst]==Integer.MAX_VALUE?-1:prices[dst];
+
+        return -1;
     }
 }
