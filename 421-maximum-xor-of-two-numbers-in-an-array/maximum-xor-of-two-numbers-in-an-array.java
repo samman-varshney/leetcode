2341caa1bbf675efdccbfb244d4429adf67437d2
Samman Varshney <sammanvarshney345@gmail.com>
2026-01-01 19:32:25 +0530

    Time: 472 ms (88.48%) | Memory: 197.5 MB (86.08%) - LeetSync

--- 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
@@ -1,57 +1,23 @@
-class TrieNode{
-    TrieNode[] child;
-    public TrieNode(){
-        child = new TrieNode[2];
-    }
-}
-
-class Trie{
-    TrieNode root;
-    public Trie(){
-        root = new TrieNode();
-    }
-
-    void insert(int num){
-        TrieNode node = root;
-        for(int i=31; i>=0; i--){
-            int bit = ((num>>i)&1);
-            if(node.child[bit] == null){
-                node.child[bit] = new TrieNode();
-            }
-            node = node.child[bit];
-        }
-    }
-
-    int getMaxXor(int num){
-        int res = 0;
-        TrieNode node = root;
-        for(int i=31; i>=0; i--){
-            int bit = ((num>>i)&1);
-            int opposite = bit^1;
-
-            if(node.child[opposite] != null){
-                res |= (1<<i);
-                node = node.child[opposite];
-            }else{
-                node = node.child[bit];
-            }
-        }
-        return res;
-    }
-
-}
-
 class Solution {
     public int findMaximumXOR(int[] nums) {
-        Trie t = new Trie();
-        for(int x: nums){
-            t.insert(x);
-        }
+        HashSet<Integer> prefix_set = new HashSet<>();
+        int mask = 0;
+        int result = 0;
+        for(int i=31; i>=0; i--){
+            mask = mask | (1<<i);
+            for(int num: nums){
+                prefix_set.add(mask&num);
+            }
 
-        int max = Integer.MIN_VALUE;
-        for(int x: nums){
-            max = Math.max(max, t.getMaxXor(x));
+            int temp = result | (1<<i);
+            for(int prefix: prefix_set){
+                if(prefix_set.contains(prefix^temp)){
+                    result = temp;
+                    break;
+                }
+            }
+            prefix_set.clear();
         }
-        return max;
+        return result;
     }
 }
