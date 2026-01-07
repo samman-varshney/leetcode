2643887ed3338cf7644730c8d2c7a18ef876c633
Samman Varshney <sammanvarshney345@gmail.com>
2026-01-07 14:36:00 +0530

    Time: 12 ms (12.12%) | Memory: 94.6 MB (55.76%) - LeetSync

--- 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.java
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,38 +1,26 @@
 class Solution {
-    int n;
-    int[] nums;
-    int[] dp;
-    boolean cond1(int i){
-        return i+1 < n && nums[i] == nums[i+1] && helper(i+2);
-    }
-    boolean cond2(int i){
-        return i+2 < n && (nums[i] == nums[i+1] && nums[i+1] == nums[i+2]) && helper(i+3);
-    }
-    boolean cond3(int i){
-        return i+2 < n && (nums[i]+1 == nums[i+1] && nums[i+1]+1 == nums[i+2]) && helper(i+3);
-    }
-    public boolean helper(int i){
-        if(i == n){
-            return true;
-        }
 
-        if(dp[i] != -1){
-            return dp[i] == 1;
-        }
-
-        if(cond1(i) || cond2(i) || cond3(i)){
-            dp[i] = 1;
-            return true;
-        }
-        dp[i] = 0;
-        return false;
-
+    boolean cond1(int n, int[] nums, int i, boolean[] dp){
+        return i+1 < n && nums[i] == nums[i+1] && dp[i+2];
     }
+    boolean cond2(int n, int[] nums, int i, boolean[] dp){
+        return i+2 < n && (nums[i] == nums[i+1] && nums[i+1] == nums[i+2]) && dp[i+3];
+    }
+    boolean cond3(int n, int[] nums, int i, boolean[] dp){
+        return i+2 < n && (nums[i]+1 == nums[i+1] && nums[i+1]+1 == nums[i+2]) && dp[i+3];
+    }
+
     public boolean validPartition(int[] nums) {
-        this.n = nums.length;
-        this.nums = nums;
-        this.dp = new int[n];
-        Arrays.fill(dp, -1);
-        return helper(0);
+        int n = nums.length;
+        boolean[] dp = new boolean[n+1];
+        Arrays.fill(dp, false);
+        dp[n] = true;
+        
+        for(int i=n-1; i>=0; i--){
+            if(cond1(n, nums, i, dp) || cond2(n, nums, i, dp) || cond3(n, nums, i, dp)){
+                dp[i] = true;
+            }
+        }
+        return dp[0];
     }
 }
