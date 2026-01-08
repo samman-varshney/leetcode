625bb5b699972508af16210d1eda24b9fea0cf2c
Samman Varshney <sammanvarshney345@gmail.com>
2026-01-08 10:45:13 +0530

    Time: 24 ms (7.41%) | Memory: 50.8 MB (5.19%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
@@ -1,36 +1,28 @@
 class Solution {
-    int n, m;
-    int[] nums1, nums2;
-    int[][][] dp;
-    public int helper(int i, int j, int empty){
-        if(i>=n || j>=m){
-            return empty == 0? -(int)1e9: 0;
-        }
-        if(dp[i][j][empty]!=-1){
-            return dp[i][j][empty];
-        }
-        //take both
-        int both = nums1[i] * nums2[j] + helper(i+1, j+1, 1);
-
-        //take i but not j
-        int iButNotJ = helper(i, j+1, empty);
-
-        //take j but not i
-        int jButNotI = helper(i+1, j, empty);
-
-        return dp[i][j][empty] = Math.max(both, Math.max(iButNotJ, jButNotI));
-    }
     public int maxDotProduct(int[] nums1, int[] nums2) {
-        this.n = nums1.length;
-        this.m = nums2.length;
-        this.nums1 = nums1;
-        this.nums2 = nums2;
-        dp = new int[n][m][2];
-        for(int[][] x: dp){
-            for(int[] y: x){
-                Arrays.fill(y, -1);
+        int n = nums1.length, m = nums2.length;
+        int[][][] dp = new int[n+1][m+1][2];
+        for(int i=0; i<=m; i++){
+            dp[n][i][0] = -(int)1e9;
+            dp[n][i][1] = 0;
+        }
+        for(int i=0; i<=n; i++){
+            dp[i][m][0] = -(int)1e9;
+            dp[i][m][1] = 0;
+        }
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                for(int k=1; k>=0; k--){
+                    int both = nums1[i] * nums2[j] + dp[i+1][j+1][1];
+                    int iButNotJ = dp[i][j+1][k];
+                    int jButNotI = dp[i+1][j][k];
+
+                    dp[i][j][k] = Math.max(both, Math.max(iButNotJ,jButNotI));
+                }
             }
         }
-        return helper(0, 0, 0);
+
+        return dp[0][0][0];
     }
 }
