8f1b680ceb485fed75564c5a29e42a79e11df0a0
Samman Varshney <sammanvarshney345@gmail.com>
2026-01-13 00:04:29 +0530

    Time: 20 ms (91.05%) | Memory: 79.8 MB (47.28%) - LeetSync

--- 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.java
+++ 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.java
@@ -1,10 +1,10 @@
 class Solution {
     public int eliminateMaximum(int[] dist, int[] speed) {
         int n = dist.length;
-        double[] time = new double[n];
+        int[] time = new int[n];
 
         for(int i=0; i<n; i++){
-            time[i] = (double)dist[i]/speed[i];
+            time[i] = (dist[i] + speed[i] -1)/speed[i];
         }
         Arrays.sort(time);
         int count = 0;
