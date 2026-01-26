dbb6dca77ea29019d02dc108c90013a5f760a829
Samman Varshney <sammanvarshney345@gmail.com>
2026-01-26 16:54:37 +0530

    Time: 140 ms (32.14%) | Memory: 56.7 MB (28.54%) - LeetSync

--- 989-largest-component-size-by-common-factor/largest-component-size-by-common-factor.java
+++ 989-largest-component-size-by-common-factor/largest-component-size-by-common-factor.java
@@ -19,23 +19,13 @@ class Solution {
         }
 
     }
-    // Source - https://stackoverflow.com/a/19738969
-// Posted by Jack, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-01-26, License - CC BY-SA 3.0
-
-int gcd(int a, int b)
-{
-    int temp;
-    while (b != 0)
-    {
-        temp = a % b;
-
-        a = b;
-        b = temp;
+    void helper(HashMap<Integer, Integer> map, int j, int i, int[] nums){
+        if(map.containsKey(j)){
+            union(map.get(j), i);
+        }else{
+            map.put(j, i);
+        }
     }
-    return a;
-}
-
     public int largestComponentSize(int[] nums) {
         int n = nums.length;
         parent = new int[n];
@@ -50,23 +40,11 @@ int gcd(int a, int b)
         for(int i=0; i<n; i++){
             for(int j=2; j*j<=nums[i]; j++){
                 if(nums[i]%j == 0){
-                    if(map.containsKey(j)){
-                        union(map.get(j), i);
-                    }else{
-                        map.put(j, i);
-                    }
-                    if(map.containsKey(nums[i]/j)){
-                        union(map.get(nums[i]/j), i);
-                    }else{
-                        map.put(nums[i]/j, i);
-                    }
+                    helper(map, j, i, nums);
+                    helper(map, nums[i]/j, i, nums);
                 }
             }
-            if(map.containsKey(nums[i])){
-                union(map.get(nums[i]), i);
-            }else{
-                map.put(nums[i], i);
-            }
+            helper(map, nums[i], i, nums);
         }
 
         
