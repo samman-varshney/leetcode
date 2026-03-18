cd6284ff072dcbf541aba9b6e61b612f53fec62a
Samman Varshney <sammanvarshney345@gmail.com>
2026-03-18 08:15:55 +0530

    Time: 5 ms (80.65%) | Memory: 172.7 MB (8.06%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -2,18 +2,15 @@ class Solution {
     public int countSubmatrices(int[][] grid, int k) {
         int n = grid.length, m = grid[0].length;
         long[][] prefix = new long[n+1][m+1];
-        long[][] vertical = new long[n+1][m+1];
 
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
-                vertical[i][j] = vertical[i-1][j] + grid[i-1][j-1];
-                prefix[i][j] = vertical[i][j] + prefix[i][j-1];
+                prefix[i][j] = grid[i-1][j-1] + prefix[i][j-1] + (prefix[i-1][j] - prefix[i-1][j-1]);
             }
         }
 
         int count = 0;
         for(int i=0; i<n; i++){
-            if(prefix[i+1][1] > k)break;
             for(int j=0; j<m; j++){
                 if(prefix[i+1][j+1] > k)break;
                 count++;
