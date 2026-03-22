da3c3709c506c71bdce178d9f9005cc0b299447f
Samman Varshney <sammanvarshney345@gmail.com>
2026-03-22 18:32:50 +0530

    Time: 8 ms (99.75%) | Memory: 102.4 MB (98.02%) - LeetSync

--- 4222-count-good-subarrays/count-good-subarrays.java
+++ 4222-count-good-subarrays/count-good-subarrays.java
@@ -4,16 +4,6 @@ class Solution {
         int[] left = new int[n];
         int[] right = new int[n];
 
-        for(int i=0; i<n; i++){
-            int prev = i-1;
-            while(prev >= 0 && nums[prev] != nums[i] &&
-                  (nums[i]|nums[prev]) == nums[i])
-            {
-                prev = left[prev];
-            }
-            left[i] = prev;
-        }
-
         for(int i=n-1; i>=0; i--){
             int next = i+1;
             while(next<n && (nums[i]|nums[next]) == nums[i])
@@ -23,6 +13,13 @@ class Solution {
 
         long res = 0;
         for(int i=0; i<n; i++){
+            int prev = i-1;
+            while(prev >= 0 && nums[prev] != nums[i] &&
+                  (nums[i]|nums[prev]) == nums[i])
+            {
+                prev = left[prev];
+            }
+            left[i] = prev;
             res += (i - left[i]) * (right[i] - i);
         }
         return res;
