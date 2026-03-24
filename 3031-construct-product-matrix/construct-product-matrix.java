7f1f7617d2b53970c42a7dde55df055c4eda7f7b
Samman Varshney <sammanvarshney345@gmail.com>
2026-03-24 10:55:40 +0530

    Time: 16 ms (10.29%) | Memory: 102.4 MB (22.55%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.java
+++ 3031-construct-product-matrix/construct-product-matrix.java
@@ -3,22 +3,11 @@ class Solution {
         int n = grid.length, m = grid[0].length;
         
         int[][] product = new int[n][m];
-        long[][] upper = new long[n][m];
         long[][] lower = new long[n][m];
 
         int mod = 12345;
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(j-1 >= 0)
-                    upper[i][j] = (upper[i][j-1] * grid[i][j])%mod;
-                else if(i-1 >= 0)
-                    upper[i][j] = (upper[i-1][m-1] * grid[i][j])%mod;
-                else{
-                    upper[i][j] = grid[i][j]%mod;
-                }
-            }
-        }
+      
         for(int i=n-1; i>=0; i--){
             for(int j=m-1; j>=0; j--){
                 if(j+1 < m)
@@ -30,13 +19,12 @@ class Solution {
                 }
             }
         }
-        
+        long prev = 1;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-               long left = j-1>=0?upper[i][j-1]:i-1>=0?upper[i-1][m-1]:1;
                long right = j+1<m?lower[i][j+1]:i+1<n?lower[i+1][0]:1;
-
-                product[i][j] = (int)((left * right)%mod); 
+                product[i][j] = (int)((prev * right)%mod); 
+                prev = (grid[i][j] * prev)%mod;
             }
         }
         return product;
