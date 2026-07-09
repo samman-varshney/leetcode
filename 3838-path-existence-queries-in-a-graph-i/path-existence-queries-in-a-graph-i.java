e136e8cb726dbb49435ac3d8b3b70bb282909432
Samman Varshney <sammanvarshney345@gmail.com>
2026-07-09 10:14:32 +0530

    Time: 10 ms (44.55%) | Memory: 165.4 MB (38.61%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
@@ -1,10 +1,16 @@
 class Solution {
-    int[] parent;
-    int[] size;
+    int[] parent, size;
 
-    void union(int u, int v){
+    public int find(int u){
+        if(parent[u] == u)
+            return u;
+        return parent[u] = find(parent[u]);
+    }
+
+    public void union(int u, int v){
         int pu = find(u);
         int pv = find(v);
+
         if(pu == pv)return;
         if(size[pu] > size[pv]){
             size[pu] += size[pv];
@@ -14,39 +20,34 @@ class Solution {
             parent[pu] = pv;
         }
     }
-
-    int find(int u){
-        if(parent[u] == u){
-            return u;
-        }
-        return parent[u] = find(parent[u]);
-    }
     public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
         parent = new int[n];
         size = new int[n];
-        for(int i =0; i<n; i++){
-            parent[i ] =i;
+
+        for(int i=0; i<n; i++){
+            parent[i] = i;
             size[i] = 1;
         }
-        int[][] dummy = new int[n][2];
-        for(int i=0; i<n; i++){
-            dummy[i][0] = i;
-            dummy[i][1] = nums[i];
-        }
-        Arrays.sort(dummy, (a, b)->(a[1] - b[1]));
-        int j = n-2;
-        for(int i=n-1; i>=0; i--){
-            while(j>=0 && Math.abs(nums[i] - nums[j]) <= maxDiff){
-                union(i, j);
-                j--;
-            }
-        }
-        int m = queries.length;
-        boolean[] res = new boolean[m];
-        for(int i=0; i<m; i++){
-            res[i] = find(queries[i][0]) == find(queries[i][1]);
-        }
-        return res;
-    }
 
+        int l = 0, r = 0;
+        while(l < n){
+            while(r < n && nums[l] + maxDiff >= nums[r])
+                r++;
+            
+            for(int i=l+1; i<r; i++){
+                union(l, i);
+            }
+
+            l = Math.max(l+1, r-1);
+            r = l;
+        }
+
+        boolean[] result = new boolean[queries.length];
+        for(int i=0; i<queries.length; i++){
+            int[] q = queries[i];
+            result[i] = find(q[0]) == find(q[1]);
+        }
+
+        return result;
+    }
 }
