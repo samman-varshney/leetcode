c12f0c56cc3702824f679a942599c36606736a3c
Samman Varshney <sammanvarshney345@gmail.com>
2026-07-11 20:12:50 +0530

    Time: 4 ms (99.67%) | Memory: 47.6 MB (58.78%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -1,5 +1,5 @@
 class Solution {
-    int[] parent, size;
+    int[] parent, size, edge;
     int find(int u){
         if(parent[u] == u)
             return u;
@@ -10,48 +10,48 @@ class Solution {
         int pu = find(u);
         int pv = find(v);
 
-        if(pu == pv)return;
+        if(pu == pv){
+            edge[pu]++;
+            return;
+        }
         if(size[pv] > size[pu]){
             size[pv] += size[pu];
             parent[pu] = pv;
+            edge[pv] += edge[pu] + 1;
         }else{
             size[pu] += size[pv];
             parent[pv] = pu;
+            edge[pu] += edge[pv] + 1;
         }
     }
     public int countCompleteComponents(int n, int[][] edges) {
         parent = new int[n];
         size = new int[n];
+        edge = new int[n];
 
         for(int i=0; i<n; i++){
             parent[i] = i;
             size[i] = 1;
         }
 
-        for(int[] edge: edges){
-            int u = edge[0];
-            int v = edge[1];
+        for(int[] ed: edges){
+            int u = ed[0];
+            int v = ed[1];
 
             union(u, v);
         }
+
+        // System.out.println(Arrays.toString(edge));
         // System.out.println(Arrays.toString(size));
-        // System.out.println(Arrays.toString(parent));
+
         int[] covered = new int[n];
         int count = 0;
         for(int i=0; i<n; i++){
             int u = find(i);
             if(covered[u] == 1)continue;
             covered[u] = 1;
-            int edgeCount = 0;
-            for(int[] edge: edges){
-                if(find(edge[0]) == u){
-                    edgeCount++;
-                }
-            }
-            //System.out.println(edgeCount);
-            // for a complete graph the number of edges are n * (n - 1)/2
 
-            if(edgeCount == size[u] * (size[u] - 1)/2)
+            if(edge[u] == size[u] * (size[u] - 1)/2)
                 count++;
         }
 
