6975b5a2aa53d29446f96c1c3083435452201f44
Samman Varshney <sammanvarshney345@gmail.com>
2026-09-06 14:02:48 +0530

    Time: 261 ms (53.59%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,9 +1,4 @@
--- Write your PostgreSQL query statement below
-SELECT name
+
+SELECT name 
 FROM Customer
-WHERE id NOT IN
-(
-SELECT id
-FROM Customer
-WHERE referee_id = 2
-)
+WHERE COALESCE(referee_id, 0) != 2;
