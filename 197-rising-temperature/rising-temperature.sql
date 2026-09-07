f8ebe9b9b954e43f407057c3078ec8bfa1cd6380
Samman Varshney <sammanvarshney345@gmail.com>
2026-09-07 15:40:30 +0530

    Time: 223 ms (70.63%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,3 +1,12 @@
-select id from
-(select id, temperature current_day_temperature, lag(temperature, 1, temperature) over(order by recordDate asc) as last_day_temperature, recordDate current_date, lag(recordDate, 1, recordDate) over(order by recordDate asc) last_date  from Weather) as e
-where e.current_day_temperature > e.last_day_temperature and e.current_date - e.last_date = 1;
+SELECT id
+FROM (
+    SELECT
+        id,
+        temperature,
+        recordDate,
+        LAG(temperature) OVER (ORDER BY recordDate) AS prev_temperature,
+        LAG(recordDate) OVER (ORDER BY recordDate) AS prev_date
+    FROM Weather
+) w
+WHERE temperature > prev_temperature
+  AND recordDate - prev_date = 1;
