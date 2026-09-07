295f07ec03bdeaefae79dfbf934e17d6210adc3e
Samman Varshney <sammanvarshney345@gmail.com>
2026-09-07 23:47:13 +0530

    Time: 384 ms (5.03%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+select s.user_id, round((sum(case when c.action = 'confirmed' then 1 else 0 end) * 1.0 / count(*))::numeric, 2) confirmation_rate
+from signups s
+left join confirmations c
+on s.user_id = c.user_id
+group by s.user_id;
