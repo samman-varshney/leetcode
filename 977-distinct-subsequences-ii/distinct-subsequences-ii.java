eb3324e78e5c8b8d6b0e2dad95cdfdf301244e4b
Samman Varshney <sammanvarshney345@gmail.com>
2026-09-08 19:56:24 +0530

    Time: 94 ms (7.56%) | Memory: 46.2 MB (17.87%) - LeetSync

---
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int distinctSubseqII(String str) {
+        int n = str.length();
+        char[] s = str.toCharArray();
+        long[] dp = new long[n+2];
+        int[] lastOccurence = new int[26];
+        Arrays.fill(lastOccurence, n);
+        dp[n] = 1;
+        long mod = 1000000007;
+
+        for(int i=n-1; i>=0; i--){
+            dp[i] = ((2l * dp[i+1])%mod - dp[lastOccurence[s[i]-'a'] + 1] + mod) %mod;
+            lastOccurence[s[i]-'a'] = i;
+        }
+
+        for(int i =0; i<=n; i++){
+            System.out.println(dp[i]);
+        }
+        return (int)((dp[0] - 1 + mod) %mod);
+    }
+}
