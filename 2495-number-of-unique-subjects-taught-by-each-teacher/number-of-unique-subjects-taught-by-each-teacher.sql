7aa2e67a82ed9827a5a3d0442fc1aef7d833522a
Samman Varshney <sammanvarshney345@gmail.com>
2026-09-09 20:01:58 +0530

    Time: 478 ms (96.41%) | Memory: 0B (100.00%) - LeetSync

--- 2495-number-of-unique-subjects-taught-by-each-teacher/number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,10 +1,3 @@
-with ranked as (
-    select 
-        *,
-        dense_rank() over(partition by teacher_id order by subject_id asc) cnt
-    from teacher
-)
-
-select teacher_id, max(cnt) cnt
-from ranked
+select teacher_id, count( distinct subject_id) cnt
+from teacher
 group by teacher_id;
