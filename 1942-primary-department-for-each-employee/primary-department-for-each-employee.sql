2a38901b359adfa8ec06e9274460a5364f73181e
Samman Varshney <sammanvarshney345@gmail.com>
2026-09-10 16:01:48 +0530

    Time: 371 ms (5.03%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,10 +1,4 @@
 -- Write your PostgreSQL query statement below
-select e.employee_id, e.department_id 
-from 
-(
-    select 
-        *,
-        rank() over(partition by employee_id order by primary_flag desc) 
-    from employee
-) e
-where rank = 1;
+select distinct on (employee_id) employee_id, department_id
+from employee
+order by employee_id, primary_flag desc;
