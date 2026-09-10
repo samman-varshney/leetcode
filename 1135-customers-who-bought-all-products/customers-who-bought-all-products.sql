db5d7bde229408dcdedbc1f8802cd2c9f91b9bbd
Samman Varshney <sammanvarshney345@gmail.com>
2026-09-10 15:01:05 +0530

    Time: 439 ms (96.86%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,14 +1,6 @@
--- Write your PostgreSQL query statement below
-with ranked_customer as (
-    select 
-        customer_id,
-        count( distinct product_key) product_bought
-    from customer
-    where product_key is not null
-    group by customer_id
-)
-
 select 
     customer_id
-from ranked_customer
-where product_bought = (select count(*) from product);
+from customer
+where product_key is not null
+group by customer_id
+having count(distinct product_key) = (select count(*) from product);
