dd0486a86d5aa597dcf5a56fc94b14bd9ddf6839
Samman Varshney <sammanvarshney345@gmail.com>
2026-09-10 16:43:01 +0530

    Time: 245 ms (96.27%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,6 +1,6 @@
 with temp as (
     select *,
-    lag(id, 2, id) over(partition by num order by id) third
+    lag(id, 2) over(partition by num order by id) third
     from logs
 )
 select distinct num "ConsecutiveNums"
