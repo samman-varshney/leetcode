dce8062433392a26d02e90375203b83f025f7ef4
Samman Varshney <sammanvarshney345@gmail.com>
2026-09-11 10:07:56 +0530

    Time: 734 ms (7.36%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,15 +1,13 @@
-with cumulative_queue as (
-    select
-        *,
-        sum(weight) over(order by turn) as cumulative_weight
-    from queue
-)
-select
-    person_name
-from cumulative_queue
-where cumulative_weight = (
-    select 
-        max(cumulative_weight) 
-    from cumulative_queue 
-    where cumulative_weight <= 1000
-    );
+
+SELECT 
+    (
+        select
+            q3.person_name
+        from queue q3
+        where q3.turn = q1.turn
+    )
+FROM Queue q1 JOIN Queue q2 ON q1.turn >= q2.turn
+GROUP BY q1.turn
+HAVING SUM(q2.weight) <= 1000
+ORDER BY SUM(q2.weight) DESC
+LIMIT 1
