f7760e321350b3ac22906b807254d419da0b1157
Samman Varshney <sammanvarshney345@gmail.com>
2026-09-11 19:18:06 +0530

    Time: 187 ms (60.53%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,15 +1,11 @@
-# Write your MySQL query statement below
+
 select
 id,
 case
     when id%2 = 0
-        then previous
-        else next
-    end student
+        then coalesce((select s2.student from seat s2 where s2.id + 1 = s1.id), student)
+        else coalesce((select s3.student from seat s3 where s3.id - 1 = s1.id), student)
+    end as student
 from
-(select
-*,
-lag(student, 1, student) over(order by id) previous,
-lead(student, 1, student) over(order by id) next
-from seat) s
+seat s1
 order by id;
