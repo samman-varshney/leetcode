3378922838fa7b196b09053368b0e64627fc5dc3
Samman Varshney <sammanvarshney345@gmail.com>
2026-09-11 11:06:25 +0530

    Time: 198 ms (77.03%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -3,15 +3,10 @@
 select employee_id
 from employees
 where salary < 30000
-and manager_id in(
-    select
-    manager_id
-    from employees
-    where manager_id not in (
+and manager_id not in  (
         select
         employee_id
         from 
         employees
-    )
 )
 order by employee_id;
