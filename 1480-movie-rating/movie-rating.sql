08e4c50dbcec203d906ea77ec2e5ffe524355825
Samman Varshney <sammanvarshney345@gmail.com>
2026-09-11 19:41:51 +0530

    Time: 252 ms (67.21%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1480-movie-rating/movie-rating.sql
@@ -0,0 +1,27 @@
+
+        select name results from (select
+        u.name,
+        count(*) attempt
+        from movierating m
+        join users u
+        on m.user_id = u.user_id
+        group by u.name
+        order by attempt desc, u.name asc
+        limit 1)
+        union all
+        select 
+            title
+        from
+        (select
+        m.title,
+        avg(mr.rating) average_rating
+        from movierating mr
+        join movies m
+        on mr.movie_id = m.movie_id
+        where created_at >= '2020-02-01'
+        and created_at <= '2020-02-29'
+        group by m.title
+        order by average_rating desc, m.title asc
+        limit 1);
+
+
