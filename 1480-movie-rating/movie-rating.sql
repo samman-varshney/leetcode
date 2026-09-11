b15d5fea638cfc7fa46e601173cd6c91aff1c3e3
Samman Varshney <sammanvarshney345@gmail.com>
2026-09-11 19:48:23 +0530

    Time: 310 ms (20.52%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,27 +1,24 @@
 
-        select name results from (select
-        u.name,
-        count(*) attempt
-        from movierating m
-        join users u
-        on m.user_id = u.user_id
-        group by u.name
-        order by attempt desc, u.name asc
-        limit 1)
-        union all
-        select 
-            title
-        from
         (select
-        m.title,
-        avg(mr.rating) average_rating
+        u.name results
+        from movierating mr
+        join users u
+        on mr.user_id = u.user_id
+        group by u.name
+        order by count(*) desc, u.name asc
+        limit 1)
+
+        union all
+
+        (select
+        m.title
         from movierating mr
         join movies m
         on mr.movie_id = m.movie_id
         where created_at >= '2020-02-01'
         and created_at <= '2020-02-29'
         group by m.title
-        order by average_rating desc, m.title asc
+        order by avg(rating) desc, m.title asc
         limit 1);
 
 
