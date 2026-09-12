3326af94bbf25f880ad6be51653f1039299bf0cc
Samman Varshney <sammanvarshney345@gmail.com>
2026-09-13 01:14:40 +0530

    Time: 174 ms (84.92%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,11 +1,11 @@
 select
-    ra1.requester_id id,
-    count(*) num
-from (select distinct requester_id from requestaccepted union select distinct accepter_id from requestaccepted) ra1
-join requestaccepted ra2
-on ra1.requester_id = ra2.accepter_id
-or ra1.requester_id = ra2.requester_id
-group by ra1.requester_id
-order by count(*) desc
+    ra1.id,
+    count(id) num
+from (
+    select  requester_id id from requestaccepted 
+    union all
+    select  accepter_id id from requestaccepted) ra1
+group by id
+order by count(id) desc
 limit 1;
 
