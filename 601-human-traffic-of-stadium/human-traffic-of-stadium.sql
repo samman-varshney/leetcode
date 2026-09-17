6d0cdccd4bd5e03606c49422f3d67ec1f999c636
Samman Varshney <sammanvarshney345@gmail.com>
2026-09-17 12:45:15 +0530

    Time: 256 ms (11.71%) | Memory: 0B (100.00%) - LeetSync

---
+++ 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -0,0 +1,42 @@
+-- Write your PostgreSQL query statement below
+with t as (
+    select
+    *,
+    lag(id, 1) over(order by id) previous,
+    lead(id, 1) over(order by id) next
+    from stadium s
+    where people >= 100
+),
+
+w as (
+    select
+    *,
+    id = previous + 1 and id = next - 1 as consecutive
+    from t
+),
+
+z as (
+    select
+    *,
+    lead(consecutive, 1) over(order by id) next_consecutive,
+    lag(consecutive, 1) over(order by id) previous_consecutive
+    from w
+),
+
+x as (
+    select
+    *
+    from z
+    where 
+    consecutive 
+    or (previous + 1 = id and previous_consecutive)
+    or (next - 1 = id and next_consecutive)
+)
+
+select
+id,
+visit_date,
+people
+from x
+order by
+visit_date;
