52bfdbd1a132882096c3e0536f241032ad16402b
Samman Varshney <sammanvarshney345@gmail.com>
2026-09-17 12:09:02 +0530

    Time: 183 ms (90.38%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -1,11 +1,11 @@
 
 select
 request_at "Day",
-round((count(*) filter (where status != 'completed')) * 1.0 / greatest(1, count(*)), 2) "Cancellation Rate"
+round((count(*) filter (where status != 'completed')) * 1.0 / count(*), 2) "Cancellation Rate"
 from trips t
-join users c
-on  t.client_id = c.users_id and c.banned = 'No'
-join users d
-on t.driver_id = d.users_id and d.banned = 'No'
+join users client
+on  t.client_id = client.users_id and client.banned = 'No'
+join users driver
+on t.driver_id = driver.users_id and driver.banned = 'No'
 where request_at = '2013-10-01' or request_at = '2013-10-02' or request_at = '2013-10-03'
 group by request_at;
