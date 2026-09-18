e24f407d68e29a3e1628a633afc6d620cc98b0b3
Samman Varshney <sammanvarshney345@gmail.com>
2026-09-18 10:20:16 +0530

    Time: 945 ms (56.59%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -1,12 +1,13 @@
--- Write your PostgreSQL query statement below
 select
-    distinct product_id, 
-    (select product_name from product p where p.product_id = s.product_id)
-from sales s
-where sale_date between '2019-01-01' and '2019-03-31' 
-and product_id not in (
-    select 
-        distinct product_id
-    from sales s
-    where sale_date < '2019-01-01' or sale_date > '2019-03-31'
-);
+    product_id,
+    (select product_name from product where product_id = sales.product_id)
+from 
+    sales
+group by 
+    product_id
+having sum(
+    case 
+        when sale_date >= '2019-01-01' and sale_date <= '2019-03-31' then 0
+        else 1
+    end
+) = 0;
